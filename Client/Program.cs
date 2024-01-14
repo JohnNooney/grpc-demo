181e112b48b542ca632f99099a444b646507670b
John Nooney <1803534@uad.ac.uk>
2024-01-14 20:05:05 +0000

    Add ShapesService to clien

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,15 +1,20 @@
 ﻿using System.Threading.Tasks;
 using Grpc.Net.Client;
 using Client;
+using System.Diagnostics.CodeAnalysis;
 
-// The port number must match the port of the gRPC server.
 using var channel = GrpcChannel.ForAddress("https://localhost:7104");
 
-var client = new Greeter.GreeterClient(channel);
+// var client = new Greeter.GreeterClient(channel);
+//     var reply = await client.SayHelloAsync(
+//                     new HelloRequest { Name = "John" });
 
-var reply = await client.SayHelloAsync(
-                  new HelloRequest { Name = "John" });
+//     Console.WriteLine("Greeting: " + reply.Message);
+//     //Console.WriteLine("Press any key to exit...");
+//     //Console.ReadKey();
 
-Console.WriteLine("Greeting: " + reply.Message);
-Console.WriteLine("Press any key to exit...");
-Console.ReadKey();
+Shape square = new Shape { Type = "square", Dimension1 = 2 };
+ShapeSender shapeSenderClient = new ShapeSender(square ,channel);
+await shapeSenderClient.send();
+
+
---
+++ Client/ShapeSender.cs
@@ -0,0 +1,22 @@
+using Client;
+using Grpc.Net.Client;
+
+public class ShapeSender
+{
+    private Shape shape;
+    private GrpcChannel channel;
+
+    public ShapeSender(Shape shape, GrpcChannel channel)
+    {
+        this.shape = shape;
+        this.channel = channel;
+    }
+
+    public async Task send()
+    {
+        var client = new AreaCalculator.AreaCalculatorClient(channel);
+        var reply = await client.CalculateAreaAsync(shape);
+
+        Console.WriteLine(shape.Type + " has an area of: " + reply.Area);
+    }
+}
