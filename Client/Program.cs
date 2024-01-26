5d4f7249679aeb13f3dd37875f1f76ecb98e32ed
John Nooney <1803534@uad.ac.uk>
2024-01-26 17:24:13 +0000

    Update client to accept SSL certificates

--- Client/Program.cs
+++ Client/Program.cs
@@ -4,13 +4,18 @@ using Client;
 using System.Diagnostics.CodeAnalysis;
 
 class Program{
+
     static async Task Main(string[] args)
     {
-        string grpcAddress = Environment.GetEnvironmentVariable("GRPC_SERVER_ADDRESS") ?? "https://localhost:5001";
+        string grpcAddress = Environment.GetEnvironmentVariable("GRPC_SERVER_ADDRESS") ?? "https://localhost";
         
-        using var channel = GrpcChannel.ForAddress(grpcAddress);
+        GrpcChannelHelper channelHelper = new GrpcChannelHelper();
+
+        using var channel = GrpcChannel.ForAddress(grpcAddress, 
+            new GrpcChannelOptions { HttpHandler = channelHelper.GetHttpClientHandler()});
 
         Shape square = new Shape { Type = "square", Dimension1 = 2 };
+
         ShapeSender shapeSenderClient = new ShapeSender(square, channel);
         await shapeSenderClient.send();
     }
