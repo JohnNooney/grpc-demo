881552fd03c67b678f14e6d7aec4ebdfe02f8fc4
John Nooney <1803534@uad.ac.uk>
2024-01-27 23:14:43 +0000

    Update to use http be default

--- Client/src/Program.cs
+++ Client/src/Program.cs
@@ -4,7 +4,7 @@ using Client;
 using System.Diagnostics.CodeAnalysis;
 
 class Program{
-    static private string grpcAddress = Environment.GetEnvironmentVariable("GRPC_SERVER_ADDRESS") ?? "https://localhost";
+    static private string grpcAddress = Environment.GetEnvironmentVariable("GRPC_SERVER_ADDRESS") ?? "http://localhost";
     
     static async Task Main(string[] args)
     {
